635a5c27bf5100eb97967ddb4c51a3b1b8533909
Francisco-Fredes <francisco.a.fredes@gmail.com>
2024-08-26 19:17:12 -0400

    Agregando funciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
+// Función para encriptar el texto
+function encriptar() {
+    const texto = document.getElementById('textoIngresado').value.toLowerCase();
+    let textoEncriptado = texto
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
 
-function encriptar(){
-    alert("Encriptando")
+    document.getElementById('mensajeFinal').value = textoEncriptado;
+}
 
-};
-function desencriptar(){
-    alert("Deencriptando")
+// Función para desencriptar el texto
+function desencriptar() {
+    const texto = document.getElementById('textoIngresado').value.toLowerCase();
+    let textoDesencriptado = texto
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
 
-};
+    document.getElementById('mensajeFinal').value = textoDesencriptado;
+}
